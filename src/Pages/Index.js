5c0637f31295d66d271d7ad96c3e4b80bc62724e
SangUn Park <Sangunpark1@gmail.com>
2023-04-17 13:25:21 -0400

    adding the Index Page

---
+++ src/Pages/Index.js
@@ -0,0 +1,11 @@
+import Transactions from "../Components/Transactions"
+
+export default function Index(){
+
+    return(
+        <div className="Index">
+          <h2>Transaction Index</h2>
+            <Transactions/>
+        </div>
+    )
+};
